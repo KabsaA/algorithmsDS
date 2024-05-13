27e617e531ae264a8bd94ca8a196f303e5c2cb5f
Kabsa <kabsaabdi23@gmail.com>
2024-05-13 14:38:36 -0600

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,7 +1,11 @@
 class Node:
-  def __init__(self,value):
+  def __init__(self,value,next_node=None):
     self.value = value
+    self.next_node = next_node
   
 class LinkedList:
-  def __init__(self,next_node):
+  def __init__(self,head_node):
+    self.head_node = head_node
+
+  def printLst(self):
     pass
