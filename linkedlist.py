759ed09384d4ec95a960bcdff4172ae7ad15ec53
Kabsa <kabsaabdi23@gmail.com>
2024-05-16 03:18:08 -0600

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -2,6 +2,9 @@ class Node:
   def __init__(self,value,next_node=None):
     self.value = value
     self.next_node = next_node
+
+  def get_value(self):
+    return self.value
   
 class LinkedList:
   def __init__(self,head_node):
